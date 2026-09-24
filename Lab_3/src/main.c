45ac3b9ce69a2a557a77a107808a9ddfe6a3220c
Tirth-Thakkar <tirth1226@gmail.com>
2026-09-23 17:22:18 -0700

    More Lab 3

--- Lab_3/src/main.c
+++ Lab_3/src/main.c
@@ -5,9 +5,26 @@
 
 #define _XTAL_FREQ 64000000  // Set the system clock frequency (32 MHz for DM164136 with PIC18F48Q10)
 
-void main(void){
+uint8_t intr_cnt = 0x00;
+
+void init(void){
     // Configure Oscillator 64MHz
     OSCCON = 0b01110000;
     OSCTUNE= 0b01000000;
+    
+    // Configure Outputs
+    TRISD = 0x00;
+    TRISB = 0x00;
 
+    LATD = 0x00;
+    LATB = 0x00;
+};
+
+void set_light(void){
+    uint8_t val_D = {0x09, 0x12, 0x24, 0x12};
+    uint8_t val_B = {0x24, 0x12, 0x09, 0x12};
+}
+
+void main(void){
+    init();
 }
